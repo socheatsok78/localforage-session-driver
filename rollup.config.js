48b2d2ac240cfbab25e7fe662a5ff2ae90b28867
Socheat Sok <alex@socheat.net>
2022-10-16 01:27:18 +0700

    v1.1.0

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import pkg from './package.json'
+import json from '@rollup/plugin-json'
+import commonjs from '@rollup/plugin-commonjs';
+// import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+
+function config({ name, input, format, minify, ext = "js" }) {
+    const dir = `dist/${format}`;
+    const minifierSuffix = minify ? ".min" : "";
+
+    return {
+        input: `src/${input}.js`,
+        output: {
+            file: `${dir}/${input}${minifierSuffix}.${ext}`,
+            name,
+            format,
+            sourcemap: true,
+        },
+        plugins: [
+            json(),
+            nodeResolve({
+                browser: true
+            }),
+            commonjs(),
+            // typescript({
+            //     tsconfig: './tsconfig.json',
+            //     declarationDir: '.'
+            // }),
+            minify
+                ? terser({
+                    compress: true,
+                    mangle: true,
+                })
+                : undefined
+        ].filter(Boolean),
+    };
+}
+
+function mapConfigs({ name, input }) {
+    return [
+        // { name, input, format: "esm", minify: false, ext: 'mjs' },
+        // { name, input, format: "esm", minify: true, ext: 'mjs' },
+        // { name, input, format: "esm", minify: false },
+        // { name, input, format: "esm", minify: true },
+        { name, input, format: "umd", minify: false },
+        { name, input, format: "umd", minify: true },
+    ]
+}
+
+export default [
+    ...mapConfigs({ name: pkg.name, input: 'index' })
+].map(config)
