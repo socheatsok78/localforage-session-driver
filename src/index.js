f9df6c634aa854d3e961cd0c23ee5c28517ff118
Socheat Sok <alex@socheat.net>
2022-10-16 00:14:11 +0700

    Rework plugin register

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import localforage from 'localforage'
 import sessionStorageWrapper from './sessionstorage'
 
-let installed = false
+// Export the SessionStorage wrapper driver
+export const SESSIONSTORAGE = sessionStorageWrapper._driver
 
+// Add driver to localforage
+localforage.SESSIONSTORAGE =SESSIONSTORAGE
+
+// Register the SessionStorage wrapper to LocalForage instance
+let installed = false
 if (!installed) {
-    // Register the SessionStorage wrapper to LocalForage instance
     localforage.defineDriver(sessionStorageWrapper);
     installed = true
 }
-
-// Export the SessionStorage wrapper driver
-export const SESSIONSTORAGE = sessionStorageWrapper._driver
